331b8d7c59edd39ebf05bb3508dc2df8db2c5865
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-08-26 11:40:40 -0400

    Intégré le style directement dans le module
    Signed-off-by: Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>

--- views/validform/validform.php
+++ views/validform/validform.php
@@ -1,5 +1,38 @@
-<div id="orphan-errors"></div>
+<style>
+    .erreurtest {
+        background: -webkit-linear-gradient(top, #d94a61, #c62a43);
+        background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#d94a61), to(#c62a43));
+        background: -moz-linear-gradient(top, #d94a61, #c62a43);
+        background: -ms-linear-gradient(top, #d94a61, #c62a43);
+        background: -o-linear-gradient(top, #d94a61, #c62a43);
+        filter: progid:DXImageTransform.Microsoft.gradient(enabled='true',
+            startColorstr=#d94a61, endColorstr=#c62a43);
+        background-color: #c62a43;
+        width:200px;
+        border-radius: 5px;
+        padding: 10px;
+        font-style: normal;
+        font-size: 12px;
+        color: #fff;
+        position:relative;
+        left: 100px;
+        top: 5px;
+    }
 
+
+
+    .erreurtest img {
+        position:absolute;
+        top: -10px;
+    }
+
+
+
+
+</style>
+
+
+<div id="orphan-errors"></div>
 
 <div style="display: none">
     <div id="erreurtest-template" style="display: none;">
