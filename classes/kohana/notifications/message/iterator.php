f631fe62860caf6f97c4b65188bfaa0d097451f6
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-12-19 14:05:19 -0500

    Doc for iterator class
    Signed-off-by: Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>

--- classes/kohana/notifications/message/iterator.php
+++ classes/kohana/notifications/message/iterator.php
@@ -3,7 +3,13 @@
 defined('SYSPATH') or die('No direct script access.');
 
 /**
+ * Iterator for messages.
  * 
+ * @see ArrayIterator
+ * 
+ * @package Notifications
+ * @author Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
+ * @copyright (c) 2012, Hète.ca Inc.
  */
 class Kohana_Notifications_Message_Iterator extends ArrayIterator {
 
