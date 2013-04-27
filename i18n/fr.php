55ca9a390b20f02a76729c1e7f3ec7009075ab19
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-04-27 13:21:38 -0400

    Added messages and translations for validations
    Signed-off-by: Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>

--- i18n/fr.php
+++ i18n/fr.php
@@ -6,7 +6,29 @@ return array(
     "notifications.error" => ":message pour le champ :field",
     "validform.formisinvalid" => "Le formulaire est invalide",
     "validform.errorforfield" => "Erreurs pour :field",
+    // Validation
+    'validation.alpha' => ':field doit contenir uniquement des lettres',
+    'validation.alpha_dash' => ':field doit contenir uniquement des lettres, des chiffres et des dash',
+    'validation.alpha_numeric' => ':field must doit contenir uniquement des lettres et des chiffres',
+    'validation.color' => ':field doit être une couleur',
+    'validation.credit_card' => ':field doit être un numéro de carte de crédit',
+    'validation.date' => ':field doit être une date',
+    'validation.decimal' => ':field doit être un nombre avec :param2 décimales',
+    'validation.digit' => ':field must be a digit',
+    'validation.email' => ':field doit être une adresse courriel',
+    'validation.email_domain' => ':field must contain a valid email domain',
+    'validation.equals' => ':field doit être égal à :param2',
+    'validation.exact_length' => ':field doit contenir exactement :param2 caractères',
+    'validation.in_array' => ':field must be one of the available options',
+    'validation.ip' => ':field doit être une adresse ip',
+    'validation.matches' => ':field doit être égal à :param2',
+    'validation.min_length' => ':field must be at least :param2 characters long',
+    'validation.max_length' => ':field must not exceed :param2 characters long',
+    'validation.not_empty' => ':field ne doit pas être vide',
+    'validation.numeric' => ':field doit être numérique',
+    'validation.phone' => ':field doit être un numéro de téléphone',
+    'validation.range' => ':field must be within the range of :param2 to :param3',
+    'validation.regex' => ':field does not match the required format',
+    'validation.url' => ':field doit être un url',
 );
-
-
 ?>
---
+++ messages/validation.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+
+return array(
+    'alpha' => 'validation.alpha',
+    'alpha_dash' => 'validation.alpha_dash',
+    'alpha_numeric' => 'validation.alpha_numeric',
+    'color' => 'validation.color',
+    'credit_card' => 'validation.credit_card',
+    'date' => 'validation.date',
+    'decimal' => 'validation.decimal',
+    'digit' => 'validation.digit',
+    'email' => 'validation.email',
+    'email_domain' => 'validation.email_domain',
+    'equals' => 'validation.equals',
+    'exact_length' => 'validation.exact_length',
+    'in_array' => 'validation.in_array',
+    'ip' => 'validation.ip',
+    'matches' => 'validation.matches',
+    'min_length' => 'validation.min_length',
+    'max_length' => 'validation.max_length',
+    'not_empty' => 'validation.not_empty',
+    'numeric' => 'validation.numeric',
+    'phone' => 'validation.phone',
+    'range' => 'validation.range',
+    'regex' => 'validation.regex',
+    'url' => 'validation.url',
+);
+?>
